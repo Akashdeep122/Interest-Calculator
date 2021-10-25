a4516001548eb39a1bef972d44e79684da248261
Akashdeep <akashdeep1k@gmail.com>
2021-10-25 20:05:09 +0530

    Snapshot 1w3a Renamed Important Files

--- STOPI3.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn=sqlite3.connect("warnings.db")
-cur=conn.cursor()
-cur.execute("CREATE TABLE warns (name STRING, amount INTEGER, interest REAL, final REAL)")
-conn.commit()
-conn.close()
